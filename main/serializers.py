cf01ee877aeefbccc3e20735c4bf859c17d39aab
aanastasiyatuz <aanastasiyatuz@gmail.com>
2022-12-01 11:38:37 +0600

    viewset comments

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Post
-
+from reviews.serializers import CommentSerializer
 
 class PostSerializer(ModelSerializer):
     class Meta:
@@ -8,9 +8,9 @@ class PostSerializer(ModelSerializer):
         fields = '__all__'
 
     def to_representation(self, instance: Post):
-        # print("instance:", instance)
         rep = super().to_representation(instance)
-        # print("repr:", dict(rep))
         rep["author"] = instance.author.username
+        comments = instance.comments.all()
+        rep["comments"] = CommentSerializer(comments, many=True).data
         return rep
 
